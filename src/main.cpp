96c0c20b43deb5d910016acf2069a7e0c1b3aabe
larycoder <lenhuchuhiep99@gmail.com>
2022-12-26 10:39:51 +0700

    Timetable: modulize project

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "utils.h"
 #include <cstring>
 #include <iostream>
 #include <vector>
@@ -6,74 +7,16 @@
 
 using namespace std;
 
-typedef struct cell {
-    int rowSize;
-    int colSize;
-} Cell;
-
 char buffer[MAX];
-
-void debug(string msg)
-{
-    cout << "[DEBUG] " << msg << endl;
-}
-
-char* get(char const* const string)
-{
-    char* p = (char*)malloc(strlen(string));
-    memcpy(p, string, strlen(string) + 1);
-    return p;
-}
-
-void drawTable(vector<char*> data, vector<Cell> cellInfo, int num, Cell table)
-{
-    debug("table: " + to_string(table.rowSize) + ", " + to_string(table.colSize));
-    for (int row = 0; row < table.rowSize; row++)
-        for (int col = 0; col < table.colSize; col++) {
-            // debug(to_string(row) + to_string(col));
-            if (col == 0)
-                strcat(buffer, "| ");
-            else if (col == (table.colSize - 1))
-                strcat(buffer, " |\n");
-            else if (row == 0 || row == (table.rowSize - 1))
-                strcat(buffer, "-");
-            else
-                strcat(buffer, "#");
-        }
-}
 
 /**
  * data: list of values
- * num: number of value in single row
+ * row_len: number of value in single row
  *
  */
-void build(vector<char*> data, int num)
+void build(vector<char*> data, int row_len)
 {
-    vector<Cell> cellInfo;
-
-    // analyze step
-    int rowSize, colSize;
-    for (int i = 0; i < data.size(); i++) {
-        int row = i / num;
-        int col = i % num;
-
-        rowSize = 1; // default
-        colSize = 0;
-        for (int j = 0; j < data.size(); j++)
-            if (j % num == col && strlen(data.at(j)) > colSize)
-                colSize = strlen(data.at(j));
-
-        cellInfo.push_back({ rowSize, colSize });
-    }
-
-    // table size computation
-    Cell table = { (int)cellInfo.size() / num + 2, 0 };
-    for (int i = 0; i < num; i++) {
-        table.colSize += cellInfo.at(i).colSize + 1;
-    }
-
-    // draw table
-    drawTable(data, cellInfo, num, table);
+    draw_table(buffer, 5, 10);
 }
 
 void test()
---
+++ src/utils.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void debug(std::string msg);
+char* get(char const* const string);
+void draw_table(char* buffer, int row, int col);
